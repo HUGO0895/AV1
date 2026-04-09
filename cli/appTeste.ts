40ed93cea0141483706d3626e109ae2002271511
Hugo Leo <hugoleonardo040906@gmail.com>
2026-04-09 17:44:58 -0300

    pode acento ;

--- cli/appTeste.ts
+++ cli/appTeste.ts
@@ -10,20 +10,20 @@ export default class AppTeste{
     public async adicionar(){
         const id= this.entrada.receberTexto("Id de aeronave valido:")
         const tipo=this.entrada.receberTexto("Tipo do Teste(Eletrico,hidraulico,aerodinamico):")
-        const tratamentoTipo= tipo.toLowerCase()=="eletrico" ? TipoTeste.ELETRICO:tipo.toLocaleLowerCase()=="hidraulico" ? TipoTeste.HIDRAULICO:TipoTeste.AERODINAMICO
+        const tratamentoTipo= tipo.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase()=="eletrico" ? TipoTeste.ELETRICO:tipo.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLocaleLowerCase()=="hidraulico" ? TipoTeste.HIDRAULICO:TipoTeste.AERODINAMICO
         const resultado=this.entrada.receberTexto("Resultado:").toLowerCase()=="aprovado" ? Resultado.APROVADO:Resultado.REPROVADO
         await this.testeService.create(new Teste(tratamentoTipo,resultado),id) // Pastel 
     }
     public async deletar(){
          const id= this.entrada.receberTexto("Id de aeronave valido:")
          const tipo=this.entrada.receberTexto("Tipo do Teste(Eletrico,hidraulico,aerodinamico):")
-        const tratamentoTipo= tipo.toLowerCase()=="eletrico" ? TipoTeste.ELETRICO:tipo.toLocaleLowerCase()=="hidraulico" ? TipoTeste.HIDRAULICO:TipoTeste.AERODINAMICO
+        const tratamentoTipo= tipo.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase()=="eletrico" ? TipoTeste.ELETRICO:tipo.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLocaleLowerCase()=="hidraulico" ? TipoTeste.HIDRAULICO:TipoTeste.AERODINAMICO
         await this.testeService.remover(tratamentoTipo,id)
     }
     public async put(){
         const id= this.entrada.receberTexto("Id de aeronave valido:")
           const tipo=this.entrada.receberTexto("Tipo do Teste(Eletrico,hidraulico,aerodinamico):")
-        const tratamentoTipo= tipo.toLowerCase()=="eletrico" ? TipoTeste.ELETRICO:tipo.toLocaleLowerCase()=="hidraulico" ? TipoTeste.HIDRAULICO:TipoTeste.AERODINAMICO
+        const tratamentoTipo=tipo.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase()=="eletrico" ? TipoTeste.ELETRICO:tipo.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLocaleLowerCase()=="hidraulico" ? TipoTeste.HIDRAULICO:TipoTeste.AERODINAMICO
          const resultado=this.entrada.receberTexto("Resultado:").toLowerCase()=="aprovado" ? Resultado.APROVADO:Resultado.REPROVADO
          await this.testeService.put(resultado,tratamentoTipo,id)
     }
